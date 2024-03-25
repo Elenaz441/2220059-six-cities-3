2c4f93162addd8ba788bc3c5f7aacb8a12c024c1
EZ <ezasypkina03@list.ru>
2024-03-26 02:19:39 +0500

    Задание 4-ого модуля

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -1,24 +1,82 @@
 import { Command } from './command.interface.js';
 import { TSVFileReader } from '../../shared/libs/file-reader/index.js';
-import { createOffer, getErrorMessage } from '../../shared/helpers/index.js';
 import chalk from 'chalk';
+import { createOffer, getErrorMessage, getMongoURI } from '../../shared/helpers/index.js';
+import { UserService } from '../../shared/models/user/user-service.interface.js';
+import { DefaultRentOfferService, RentOfferModel, RentOfferService } from '../../shared/models/rent-offer/index.js';
+import { DatabaseClient, MongoDatabaseClient } from '../../shared/libs/database-client/index.js';
+import { Logger } from '../../shared/libs/logger/index.js';
+import { ConsoleLogger } from '../../shared/libs/logger/console.logger.js';
+import { DefaultUserService, UserModel } from '../../shared/models/user/index.js';
+import { DEFAULT_DB_PORT, DEFAULT_USER_PASSWORD } from './command.constant.js';
+import { RentOffer } from '../../shared/types/index.js';
 
 export class ImportCommand implements Command {
+
+  private userService: UserService;
+  private rentOfferService: RentOfferService;
+  private databaseClient: DatabaseClient;
+  private logger: Logger;
+  private salt: string;
+
+  constructor() {
+    this.onImportedLine = this.onImportedLine.bind(this);
+    this.onCompleteImport = this.onCompleteImport.bind(this);
+
+    this.logger = new ConsoleLogger();
+    this.rentOfferService = new DefaultRentOfferService(this.logger, RentOfferModel);
+    this.userService = new DefaultUserService(this.logger, UserModel);
+    this.databaseClient = new MongoDatabaseClient(this.logger);
+  }
+
   public getName(): string {
     return '--import';
   }
 
-  private onImportedLine(line: string) {
+  private async onImportedLine(line: string, resolve: () => void) {
     const offer = createOffer(line);
-    console.info(offer);
+    await this.saveOffer(offer);
+    resolve();
   }
 
   private onCompleteImport(count: number) {
     console.info(`${count} rows imported.`);
+    this.databaseClient.disconnect();
   }
 
-  public async execute(...parameters: string[]): Promise<void> {
-    const [filename] = parameters;
+  private async saveOffer(rentOffer: RentOffer) {
+    const user = await this.userService.findOrCreate({
+      ...rentOffer.creator,
+      password: DEFAULT_USER_PASSWORD
+    }, this.salt);
+
+    await this.rentOfferService.create({
+      title: rentOffer.title,
+      description: rentOffer.description,
+      publicationDate: rentOffer.publicationDate,
+      city: rentOffer.city,
+      preview: rentOffer.preview,
+      photos: rentOffer.photos,
+      isPremium: rentOffer.isPremium,
+      isFavourites: rentOffer.isFavourites,
+      rank: rentOffer.rank,
+      housingType: rentOffer.housingType,
+      roomsCount: rentOffer.roomsCount,
+      guestsCount: rentOffer.guestsCount,
+      price: rentOffer.price,
+      conveniences: rentOffer.conveniences,
+      creatorId: user.id,
+      commentsCount: rentOffer.commentsCount,
+      coordinates: rentOffer.coordinates
+    });
+
+  }
+
+  public async execute(filename: string, login: string, password: string, host: string, dbname: string, salt: string): Promise<void> {
+    const uri = getMongoURI(login, password, host, DEFAULT_DB_PORT, dbname);
+    this.salt = salt;
+
+    await this.databaseClient.connect(uri);
     const fileReader = new TSVFileReader(filename.trim());
 
     fileReader.on('line', this.onImportedLine);
---
+++ src/shared/models/rent-offer/dto/create-rent-offer.dto.ts
@@ -0,0 +1,21 @@
+import { Convenience, HouseingType } from '../../../types/index.js';
+
+export class CreateRentOfferDto {
+  public title: string;
+  public description: string;
+  public publicationDate: Date;
+  public city: string;
+  public preview: string;
+  public photos: string[];
+  public isPremium: boolean;
+  public isFavourites: boolean;
+  public rank: number;
+  public housingType: HouseingType;
+  public roomsCount: number;
+  public guestsCount: number;
+  public price: number;
+  public conveniences: Convenience[];
+  public creatorId: string;
+  public commentsCount: number;
+  public coordinates: [number, number];
+}
